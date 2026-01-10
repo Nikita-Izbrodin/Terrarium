bd9dab0b7cd1b04fa09cfddb43f8d7127fa3bb31
Nick Izbrodin <nizbrodin@gmail.com>
2026-01-10 18:12:25 +0000

    Used tilemap example from sfml

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,107 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+class TileMap : public sf::Drawable, public sf::Transformable
+{
+public:
+    bool load(const std::filesystem::path& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height)
+    {
+        // load the tileset texture
+        if (!m_tileset.loadFromFile(tileset))
+            return false;
+
+        // resize the vertex array to fit the level size
+        m_vertices.setPrimitiveType(sf::PrimitiveType::Triangles);
+        m_vertices.resize(width * height * 6);
+
+        // populate the vertex array, with two triangles per tile
+        for (unsigned int i = 0; i < width; ++i)
+        {
+            for (unsigned int j = 0; j < height; ++j)
+            {
+                // get the current tile number
+                const int tileNumber = tiles[i + j * width];
+
+                // find its position in the tileset texture
+                const int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
+                const int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
+
+                // get a pointer to the triangles' vertices of the current tile
+                sf::Vertex* triangles = &m_vertices[(i + j * width) * 6];
+
+                // define the 6 corners of the two triangles
+                triangles[0].position = sf::Vector2f(i * tileSize.x, j * tileSize.y);
+                triangles[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
+                triangles[2].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
+                triangles[3].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
+                triangles[4].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
+                triangles[5].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
+
+                // define the 6 matching texture coordinates
+                triangles[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
+                triangles[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
+                triangles[2].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
+                triangles[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
+                triangles[4].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
+                triangles[5].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
+            }
+        }
+
+        return true;
+    }
+
+private:
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const override
+    {
+        // apply the transform
+        states.transform *= getTransform();
+
+        // apply the tileset texture
+        states.texture = &m_tileset;
+
+        // draw the vertex array
+        target.draw(m_vertices, states);
+    }
+
+    sf::VertexArray m_vertices;
+    sf::Texture     m_tileset;
+};
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({512, 512}), "Terrarium", sf::Style::Close | sf::Style::Titlebar);
-    window.setVerticalSyncEnabled(true); // call it once after creating the window
-    // run the program as long as the window is open
+    // create the window
+    sf::RenderWindow window(sf::VideoMode({512, 256}), "Terrarium");
 
-    sf::Texture texture;
-    if (!texture.loadFromFile("red.png", false, sf::IntRect({0, 0}, {100, 100})))
-    {
-        // error...
-        std::cout << "File did not load." << std::endl;
-        return 1;
-    }
-    sf::Sprite sprite(texture);
+    // define the level with an array of tile indices
+    constexpr std::array level = {
+        0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0,
+        1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3,
+        0, 1, 0, 0, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 0, 0,
+        0, 1, 1, 0, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2, 0, 0,
+        0, 0, 1, 0, 3, 0, 2, 2, 0, 0, 1, 1, 1, 1, 2, 0,
+        2, 0, 1, 0, 3, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1,
+        0, 0, 1, 0, 3, 2, 2, 2, 0, 0, 0, 0, 1, 1, 1, 1,
+    };
 
+    // create the tilemap from the level definition
+    TileMap map;
+    if (!map.load("tileset.png", {32, 32}, level.data(), 16, 8))
+        return -1;
+
+    // run the main loop
     while (window.isOpen())
     {
-        // check all the window's events that were triggered since the last iteration of the loop
+        // handle events
         while (const std::optional event = window.pollEvent())
         {
-            // "close requested" event: we close the window
             if (event->is<sf::Event::Closed>())
-            {
                 window.close();
-            }
-       }
+        }
 
-       if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::W)) {
-            //std::cout << "Hello World!" << std::endl;
-            sprite.move({0.f, -2.f});
-       }
-       if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::A)) {
-            //std::cout << "Hello World!" << std::endl;
-            sprite.move({-2.f, 0.f});
-       }
-       if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::S)) {
-            //std::cout << "Hello World!" << std::endl;
-            sprite.move({0.f, 2.f});
-       }
-       if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::D)) {
-            //std::cout << "Hello World!" << std::endl;
-            sprite.move({2.f, 0.f});
-       }
-       if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Escape)) {
-            window.close();
-       }
-
-        window.clear(sf::Color::Black);
-
-       
-        window.draw(sprite);
-
+        // draw the map
+        window.clear();
+        window.draw(map);
         window.display();
     }
-
-    return 0;
 }
